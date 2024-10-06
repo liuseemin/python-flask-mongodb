febe3ff1af73ae4a2d7ac629ad1d043d332665bf
liuseemin <liuseemin@gmail.com>
2024-10-06 21:12:21 +0800

    use flask_login to manage login session, and change

--- app-security.py
+++ app-security.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from flask_pymongo import PyMongo
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask_talisman import Talisman
 from pymongo.server_api import ServerApi
 from bson.objectid import ObjectId
@@ -10,6 +10,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from pymongo.errors import DuplicateKeyError
 from flask_wtf.csrf import CSRFProtect
+import uuid
 import email_validator
 
 from config import Config
@@ -43,6 +44,46 @@ db = mongo.db
 # collection
 todos = db.todos
 users_collection = db.users
+
+# Login Manager setting
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+# define User class
+class User(UserMixin):
+    def __init__(self, id, username, email, verified, hashed_password) -> None:
+        super().__init__()
+
+        self.id = id
+        self.username = username
+        self.email = email
+        self.verified = verified
+        self.hashed_password = hashed_password
+    
+    @classmethod
+    def make_from_dict(cls, dict):
+        return cls(dict['id'], dict['username'], dict['email'], dict['verified'], dict['hashed_password'])
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'verified': self.verified,
+            'hashed_password': self.hashed_password
+        }
+
+    def get_id(self):
+        return self.id
+
+# Load user from user ID
+@login_manager.user_loader
+def load_user(userid):
+    # find user by userid and omit _id when return
+    user = users_collection.find_one({'id': userid}, {'_id': False})
+    if user:
+        return User.make_from_dict(user)
 
 # Flask-WTF form for user registration
 class RegistrationForm(FlaskForm):
@@ -82,7 +123,7 @@ def index():
 # Delete item
 @app.post('/<id>/delete/')
 def delete(id):
-    todos.delete_one({"_id":ObjectId(id)})
+    todos.delete_one({"_id": ObjectId(id)})
     return redirect(url_for('index'))
 
 # Registration page
@@ -90,13 +131,17 @@ def delete(id):
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        logout_user()
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user_data = {
+            'id': uuid.uuid4().hex, # uuid4().hex is just same without dashes
             'username': form.username.data,
             'email': form.email.data,
-            'password': hashed_password
+            'verified': True,
+            'hashed_password': hashed_password
         }
         try:
+            # new_user = User.make_from_dict(user_data)
             users_collection.insert_one(user_data)
             flash('Account created successfully', 'success')
             return redirect(url_for('login'))
@@ -108,15 +153,17 @@ def register():
 # Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'user_id' in session:
-        flash('You are already logged in!')
+    # check current user status
+    if current_user.is_authenticated:
+        flash('You are already logged in!', 'info')
         return redirect(url_for('dashboard'))
     
     form = LoginForm()
     if form.validate_on_submit():
-        user = users_collection.find_one({'email': form.email.data})
-        if user and bcrypt.check_password_hash(user['password'], form.password.data):
-            session['user_id'] = str(user['_id'])
+        user = users_collection.find_one({'email': form.email.data}, {'_id': False})
+        if user and bcrypt.check_password_hash(user['hashed_password'], form.password.data):
+            logedin_user = User.make_from_dict(user)
+            login_user(logedin_user)
             flash('Login successful!', 'success')
             return redirect(url_for('dashboard'))
         else:
@@ -126,16 +173,16 @@ def login():
 
 # Dashboard page
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    user = users_collection.find_one({'_id': ObjectId(session['user_id'])})
+    user = current_user.dict()
     return render_template('dashboard.html', username=user['username'])
 
 # Logout
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('user_id', None)
+    logout_user()
     flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
